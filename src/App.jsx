a793ef308e5c28f01aa23f83279f7522288675b1
ELIAS <elias.alasia.moreno@gmail.com>
2024-05-15 10:55:07 -0500

    Realizando segundo array y modificacion en css agregando imagen del buscador

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import Card from './Components/Card';
 
 function App() {
   const [dates, setDate] = useState([]);
+  const [filtered, setFiltered] = useState([])
 
   const getData = async () => {
     const rs = await fetch('stays.json');
     const rsJson = await rs.json();
     setDate(rsJson);
+    setFiltered(rsJson)
   };
 
   useEffect(() => {
@@ -18,9 +20,10 @@ function App() {
 
   return (
     <div className="stays-container container">
-      <Nav />
+      <Nav dates={dates} setFiltered={setFiltered} />
+     
       <ul className="listings stay-list">
-        {dates.map((stay, index) => (
+        {filtered.map((stay, index) => (
           <Card key={index} stay={stay} />
         ))}
       </ul>
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import logo from '../image/logo.png'; // Importa la imagen del logo
+import lupa from '../image/lupa.png'
 
-const Nav = () => {
+
+const Nav = ({dates, setFiltered}) => {
+  const [search, setSearch] = useState('')
+ function filterData() {
+  if (search.trim() !== '') {
+    const rs = dates.filter(dat => dat.city.toLowerCase().includes(search.toLowerCase()))
+    setFiltered(rs)
+  } else {
+    setFiltered(dates)
+  }
+  
+ }
+
   return (
     <nav>
-      <a href="#" className="logo">airbnb</a>
+      <a href="#" className="logo">
+        <img src={logo} alt="airbnb logo" className="logo-image" /> 
+      </a>
       <div className='inputs'>
-        <input className='locationInput' type="text" placeholder='LOCATION' />
+        <input className='locationInput' type="text" placeholder='LOCATION' value={search} onChange={(e)=> setSearch((e.target.value))}/>
         <input className='guestsInput' type="text" placeholder='Add Guests' />
-        <button>
-          <img src="#" alt="search" />
+        <button className='btn' onClick={filterData}>
+          <img src={lupa} alt="search" />
         </button>
       </div>
     </nav>
